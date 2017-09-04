db7455890d59145d892e96f225de6e7d396fa843
増渕佳輝 <yoshxj4@gmail.com>
2017-09-04 09:01:26 +0900

    fist commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(function() {
   var itemsCounter = 1;
   var container = document.getElementById('main');
+  var indexData;
 
   AOS.init({
     easing: 'ease-in-out-sine'
@@ -15,8 +16,9 @@ $(function() {
     var item = document.createElement('div');
     item.classList.add('aos-item');
     item.setAttribute('data-aos', 'fade-up');
-    item.innerHTML = `<a href="javascript:void(0);" data="${val.id}" class="product-show"><div class="aos-item__inner"><p class="productImage" style="background-image: url('${image}');"></p><p>${val.name}</p><p>${val.price}</p></div></a>`;
+    item.innerHTML = `<a href="javascript:void(0);" data_idx ="${i}" class="product-show"><div class="aos-item__inner"><p class="productImage" style="background-image: url('${image}');"></p><p>${val.name}</p><p>${val.price}</p></div></a>`;
     container.appendChild(item);
+
   }
 
  function indexAjax() {
@@ -30,35 +32,49 @@ $(function() {
     })
     .done(function(data){
       console.log(data[0]);
+      indexData = data;
       $.each(data,addItem)
     })
     .fail(function(){
         alert('error');
      });
  }
- function showAjax() {
+
+ $('form.put-form').on('submit', function(e) {
+    e.preventDefault();
+    var $form = $(this);
+    var fd = new FormData($form[0]);
+    var productId = $('#product-id').val();
+    var url = 'http://localhost:3000/api/v1/products/' + productId;
+    console.log(url);
     $.ajax({
-      type: 'get',
+      type: 'post',
       url: 'http://localhost:3000/api/v1/products',
+      data: fd,
       dataType: 'json',
       timeout: 10000,
+      crossDomain : true,
       processData: false,
       contentType: false
     })
     .done(function(data){
-      console.log(data[0]);
-      $.each(data,addItem)
+      location.reload()
     })
     .fail(function(){
         alert('error');
      });
- }
- 
-
+    return false;
+  });
 
  $("#main").on("click",".product-show", function(){
-    var data =  $(this).attr('data')
-
+    var idx =  $(this).attr('data_idx')
+    var productData = indexData[idx];
+    $('#product-name').val(productData.name);
+    $('#product-price').val(productData.price);
+    $('#product-id').val(productData.id);
+    $('#product-shop').val(productData.shop_id);
+    $('.modal-image').css('background-image',`url(${productData.image.url})`);
+    $('#showModal').modal();
  });
 
 
